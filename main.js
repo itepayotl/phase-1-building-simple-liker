9d961ee9e55356a6b48d79dd2cab7344c8fe153f
Israel <itepayotl@yahoo.com>
2023-09-26 15:24:47 -0700

    complete

--- main.js
+++ main.js
@@ -3,9 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeButtons = document.querySelectorAll('li.like')
+const errorModal = document.querySelector('#modal')
+const likeHearts = document.querySelectorAll('.like-glyph')
+for(const likeHeart of likeHearts){
+  likeHeart.addEventListener('click', () => {
+    if(likeHeart.textContent === FULL_HEART){
+      likeHeart.textContent = EMPTY_HEART
+      likeHeart.classList.remove('activated-heart')
+    }
+  })
+}
 
+likeButtons.forEach(likeButton => {
+  likeButton.addEventListener('click', () => {
+    mimicServerCall().then(() => {
+      const likeHearts = likeButton.querySelector('.like-glyph')
+      likeHearts.textContent = FULL_HEART
+      likeHearts.classList.add('activated-heart')
 
-
+    }).catch((error) => {
+      errorModal.classList.remove('hidden')
+      errorModal.querySelector('p#modal-message').textContent = error
+      setTimeout(() => {
+        errorModal.classList.add('hidden')
+      }, 3000)
+    })
+  })
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
